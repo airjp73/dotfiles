66663208e1037cdc26cab9a39ecaa6e9722fcd2f
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-21 13:39:57 -0400

    more plugins

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -2,5 +2,9 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<D-s>", ":w<CR>")
+keymap.set("n", "<D-s>", ":w<CR>", { desc = "Save with cmd-s" })
+keymap.set("n", "<D-a>", "ggVG", { desc = "Select all with cmd-a" })
 
+keymap.set("n", "<D-/>", "gcc", { desc = "Comment current selection", remap = true })
+keymap.set("i", "<D-/>", "<Esc>gcci", { desc = "Comment current selection", remap = true })
+keymap.set("v", "<D-/>", "gcgv", { desc = "Comment current selection", remap = true })
