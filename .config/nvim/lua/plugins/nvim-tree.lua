03efa9e5e72812d73113f5a2662af78a7d73b623
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-21 15:28:04 -0400

    commit more stuff

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,45 +1,46 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "→",
-              arrow_open = "↓",
-            }
-          }
-        },
-        -- disable window_picker for explorer to wrok well with window splits
-        actions = {
-          open_file = {
-            window_picker = {
-              enable = false,
-            }
-          }
-        },
-        filters = {
-          custom = { ".DS_Store" },
-        },
-        git = {
-          ignore = false,
-        }
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "→",
+							arrow_open = "↓",
+						},
+					},
+				},
 
-      }
-    })
-  end
+				-- These just seem to generate an error on startup, so not sure what to do with them
+				-- disable window_picker for explorer to wrok well with window splits
+				-- actions = {
+				-- 	open_file = {
+				-- 		window_picker = {
+				-- 			enable = false,
+				-- 		},
+				-- 	},
+				-- },
+				-- filters = {
+				--   custom = { ".DS_Store" },
+				-- },
+				-- git = {
+				--   ignore = false,
+				-- }
+			},
+		})
+	end,
 }
