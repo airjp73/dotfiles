dabbc7401ad14e7a00e4366a19cddf217f734361
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-06-25 08:46:17 -0400

    Fix vim surround in vscode

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,6 +1,14 @@
+local event
+
+if not vim.g.vscode then
+	event = { "BufReadPre", "BufNewFile" }
+else
+	event = { "VeryLazy" }
+end
+
 return {
-  "kylechui/nvim-surround",
-  event = { "BufReadPre", "BufNewFile" },
-  version = "*",
-  config = true
+	"kylechui/nvim-surround",
+	event = event,
+	version = "*",
+	config = true,
 }
